b3fb1e325995297b9c4e40cb8b40f61b67cefa32
chenyan <670058670@qq.com>
2020-04-03 02:15:13 +0800

    最近更改

--- gmall-user/src/main/java/com/lianxi/gmall/user/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.lianxi.gmall.user.mapper;
-
-
-import com.lianxi.gmall.user.entity.UmsMember;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserMapper extends Mapper<UmsMember> {
-}
--- gmall-user/src/main/java/com/lianxi/gmall/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.lianxi.gmall.user.service;
-
-import com.lianxi.gmall.user.entity.UmsMember;
-import java.util.List;
-
-public interface UserService {
-
-    List<UmsMember> getUserlist();
-}
